5a3b726159135e3da21abbe785e54a6f43d0b773
YaChun07 <yachun07@gmail.com>
2017-08-11 19:03:28 +0800

    DeadAnt
    DeadAnt
    1.取代掉好的螞蟻
    2.算Head Body Feet
    3.找出最多的即為死螞蟻數

---
+++ HWDeadAnt/HWDeadAnt/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWDeadAnt
+{
+    public class DeadAntClass
+    {
+
+        public static int deadAntCount(String ants)
+        {
+            int head = 0;
+            int body = 0;
+            int feet = 0;
+            int deadAntAmount = 0;
+            if (ants != null)
+            {
+                if (ants.Contains("ant"))
+                {
+                    ants = ants.Replace("ant", "");
+                }
+              
+                for (int i = 0; i < ants.Length; i++)
+                {
+
+                    if (ants.ElementAt(i).Equals('a'))
+                    {
+                        head++;
+                    }
+                    else if (ants.ElementAt(i).Equals('n'))
+                    {
+                        body++;
+                    }
+                    else if (ants.ElementAt(i).Equals('t'))
+                    {
+                        feet++;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                    
+                }          
+                deadAntAmount = Math.Max(feet, Math.Max(head, body));
+            }
+            return deadAntAmount;
+        }
+    }
+}
---
+++ HWDeadAnt/HWDeadAntTests/Class1Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HWDeadAnt;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HWDeadAnt.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void noDeadAntTest()
+        {
+            Assert.AreEqual(0, DeadAntClass.deadAntCount("ant ant ant ant"));
+            Assert.AreEqual(0, DeadAntClass.deadAntCount(null));
+            Assert.AreEqual(0, DeadAntClass.deadAntCount("fgkjhidke"));
+            
+        }
+
+
+        [TestMethod()]
+        public void OneDeadAntTest()
+        {
+            Assert.AreEqual(1, DeadAntClass.deadAntCount("ant ant .... a nt"));
+        }
+
+        [TestMethod()]
+        public void TwoDeadAntTest()
+        {
+            Assert.AreEqual(2, DeadAntClass.deadAntCount("ant anantt aantnt"));
+            Assert.AreEqual(2, DeadAntClass.deadAntCount("ant ant .... a nntt"));
+        }
+
+        [TestMethod()]
+        public void ThreeDeadAntTest()
+        {
+            Assert.AreEqual(3, DeadAntClass.deadAntCount("ant ant .... a nnttt"));
+        }
+
+
+
+    }
+}
